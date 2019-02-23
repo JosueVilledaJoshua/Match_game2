34fd12ef26ade00b84604c8856f1db7a1e1346d2
JosueVA9 <a01339544@itesm.mx>
2019-02-22 23:41:07 -0600

    Primer avance y prubas de proyecto final

---
+++ Una pelea de huevos/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("SelectCharacters");
+        Debug.Log("Play button pushed");
+    }
+
+    public void Continue()
+    {
+        SceneManager.LoadScene("Game");
+        Debug.Log("Continue button pushed");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit button pushed");
+     
+    }
+}
---
+++ Una pelea de huevos/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+
+    void Start()
+    {
+        //transform.position = new Vector3(0, 0, 0);
+    }
+
+
+    void Update()
+    {
+        MoveHorizontal();
+    }
+
+    //Fir 2 player, stablish 2 different scripts for individual commands
+    //Now has horizontal for testing purposes
+    public void MoveHorizontal()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
+
+    }
+}
